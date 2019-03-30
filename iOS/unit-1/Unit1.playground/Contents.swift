a3a324af91ed809f640c43cd0f134f78f98e95e5
Xanathos <christiantorrico@MacBook-Pro-de-Christian.local>
2019-03-30 01:28:09 -0500

    Data Mapping - Part 1

---
+++ iOS/unit-1/Unit1.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let players = [["name" : "Joe Smith", "height" : 42, "experience" : true, "guardians" : "Jim and Jan Smith"],
+               ["name" : "Jill Tanner", "height" : 36, "experience" : true, "guardians" : "Clara Tanner"],
+               ["name" : "Bill Bon", "height" : 43, "experience" : true, "guardians" : "Sara and Jenny Bon"],
+               ["name" : "Eva Gordon", "height" : 45, "experience" : false, "guardians" : "Wendy and Mike Gordon"],
+               ["name" : "Matt Gill", "height" : 40, "experience" : false, "guardians" : "Charles and Sylvia Gill"],
+               ["name" : "Kimmy Stein", "height" : 41, "experience" : false, "guardians" : "Bill and Hillary Stein"],
+               ["name" : "Sammy Adams", "height" : 45, "experience" : false, "guardians" : "Jeff Adams"]]
